119bb08c01d65e6f7213e5f4c2c9afe16db6d6da
Juanjo <juanjo.3ns@gmail.com>
2018-11-03 14:11:50 +0000

    main changes

--- src/main.py
+++ src/main.py
@@ -9,18 +9,20 @@ success = True
 
 video = cv2.VideoCapture('video1.mp4')
 success,image = video.read()
+fps = video.get(cv2.CAP_PROP_FPS)
 
 createCSV()
+
 def isTomCruise(image):
 	return random.randint(0,1)
 
 if not os.path.exists('frames'):
 	os.mkdir('frames')
+
 while success:
-	start_time = time.time()
-	cv2.imwrite("frames/frame%d.jpg" % count, image)
-	editCSV(count,isTomCruise(image))
+	if count%int(fps)==0:
+		cv2.imwrite("frames/frame%d.jpg" % count, image)
+		editCSV(count/int(fps),isTomCruise(image))
 	success,image = video.read()
-	time.sleep(1.0 - time.time() + start_time)
 	print 'Read a new frame: ', success
 	count += 1
