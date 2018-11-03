61fd691ac0127356e3cffda32bd52d9e39126813
dazcona <david.azcona@asu.edu>
2018-11-03 16:51:41 +0000

    optimize search for Tom

--- src/box.py
+++ src/box.py
@@ -1,40 +1,28 @@
-# USAGE
-# python recognize_faces_image.py --image frames/frame261.jpg --detection-method hog
-
 # import the necessary packages
 import face_recognition
 import argparse
 import cv2
 import matplotlib as plt
 
-# load the known faces and embeddings
-
-def recognize(data, image, method, count):
-	
-	# Result
-	tom = 0
+def create_boxes(data, image, method):
 
 	# load the input image and convert it from BGR to RGB
-	image = cv2.imread(image)
 	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 	# detect the (x, y)-coordinates of the bounding boxes corresponding
 	# to each face in the input image, then compute the facial embeddings
 	# for each face
-	print("[INFO] recognizing faces...")
-	boxes = face_recognition.face_locations(rgb,
-		model=method)
-	encodings = face_recognition.face_encodings(rgb, boxes)
+	boxes = face_recognition.face_locations(rgb, model=method)
+	box_encodings = face_recognition.face_encodings(rgb, boxes)
 
 	# initialize the list of names for each face detected
 	names = []
 
 	# loop over the facial embeddings
-	for encoding in encodings:
-		# attempt to match each face in the input image to our known
-		# encodings
-		matches = face_recognition.compare_faces(data["encodings"],
-			encoding)
+	for encoding in box_encodings:
+		
+		# attempt to match each face in the input image to our known encodings
+		matches = face_recognition.compare_faces(data["encodings"], encoding)
 		name = "Unknown"
 
 		# check to see if we have found a match
@@ -55,7 +43,6 @@ def recognize(data, image, method, count):
 			# votes (note: in the event of an unlikely tie Python will
 			# select first entry in the dictionary)
 			name = max(counts, key=counts.get)
-			tom = 1
 		# update the list of names
 		names.append(name)
 
@@ -67,11 +54,4 @@ def recognize(data, image, method, count):
 		cv2.putText(image, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
 			0.75, (0, 255, 0), 2)
 
-	# Save image
-	filename = '../data/test/{:04d}.png'.format(count)
-	cv2.imwrite(filename, image)
-	# show the output image
-	# cv2.imshow("Image", image)
-	# cv2.waitKey(0)
-	cv2.destroyAllWindows()
-	return tom
+	return image
--- src/main.py
+++ src/main.py
@@ -3,12 +3,13 @@
 
 import os
 import cv2
-from csveditor import create_csv, edit_csv
-from recognize_faces_image import recognize
 import time
 import argparse
 import random
 import pickle
+from csveditor import create_csv, edit_csv
+from box import create_boxes
+from recognize import recognize_face
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", required=True,
@@ -20,7 +21,7 @@ ap.add_argument("-d", "--detection-method", type=str, default="cnn",
 args = vars(ap.parse_args())
 
 # Read data
-print("[INFO] loading encodings...")
+# print("[INFO] loading encodings...")
 data = pickle.loads(open(args['encodings'], "rb").read())
 
 # Create CSV
@@ -35,24 +36,33 @@ count = 0
 fps = int(video.get(cv2.CAP_PROP_FPS))
 
 # Frames dir
-if not os.path.exists('frames'):
-	os.mkdir('frames')
+# if not os.path.exists('frames'):
+# 	os.mkdir('frames')
 
 # Loop through the frames
 while success:
 	
 	if count % fps == 0: # every second
+		
 		# Second
 		second = count / fps
+		
 		# Write frame
-		filename = os.path.join('frames', 'frame_%d.jpg' % second)
+		# filename = os.path.join('frames', 'frame_%d.jpg' % second)
+		# cv2.imwrite(filename, image)
+		
+		# Bouding Boxes
+		image_boxed = create_boxes(data, image, args["detection_method"])
+		filename = '../data/test/{:04d}.png'.format(second)
 		cv2.imwrite(filename, image)
-		# Is Tom Cruise?
-		result = recognize(data, filename, args["detection_method"], count)
+
+		# Is our subject?
+		result = recognize_face(data, image, args["detection_method"])
+		
 		# Write!
 		edit_csv(second, result)
 	
 	# Keep reading!
 	success, image = video.read()
-	print 'Read a new frame: ', success
+	# print 'Read a new frame: ', success
 	count += 1
